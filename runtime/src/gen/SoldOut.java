62d7a397e140325a34c2803b7a31cd668507e941
Felix Schwägerl <felix.schwaegerl@oth-regensburg.de>
2026-04-02 11:36:24 +0200

    Restore LLM-generated method stubs

--- runtime/src/gen/SoldOut.java
+++ runtime/src/gen/SoldOut.java
@@ -13,7 +13,8 @@ public class SoldOut extends State {
     @Override
     public void onExit() {
          // generated start
-        // generated end
+System.out.println("Machine is being refilled...");
+// generated end
     }
 
     /** @prompt add the amount of the balls variable and print a message that the Machine has been refilled to $n Gumballs */
@@ -21,7 +22,9 @@ public class SoldOut extends State {
     public void refill(int amount) {
         if (true) {
          // generated start
-        // generated end
+context.setBalls(context.getBalls() + amount);
+System.out.println("Machine has been refilled to " + context.getBalls() + " Gumballs");
+// generated end
             this.onExit();
             context.setState(new NoQuarter(context));
             context.getState().onEntry();
